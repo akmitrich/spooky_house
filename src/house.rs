e87206038c528c298680df0155852a73b609a669
akmitrich <ak.mitrich@mail.ru>
2022-06-16 08:06:36 +0300

    house device test

--- src/house.rs
+++ src/house.rs
@@ -1,5 +1,5 @@
 #![allow(unused, dead_code)]
-use std::collections::{BTreeMap, btree_map::Keys, BTreeSet};
+use std::collections::{BTreeMap, btree_map::{Keys}, BTreeSet, btree_set::Iter};
 
 type Room = BTreeSet<String>;
 #[derive(Debug)]
@@ -16,12 +16,12 @@ pub struct House {
     rooms: BTreeMap<String, Room>,
 }
 
-trait DeviceInfo {
-    fn device_info(&self) -> String;
+trait ReportState {
+    fn report_state(&self) -> String;
 }
 
 trait GetDevice {
-    fn get_device(&self, room: &str, device: &str) -> &dyn DeviceInfo;
+    fn get_device(&self, room: &str, device: &str) -> &dyn ReportState;
 }
 
 impl House {
@@ -43,6 +43,18 @@ impl House {
 
     pub fn get_room_name_list(&self) -> Keys<String, Room> {
         self.rooms.keys()
+    }
+
+    pub fn add_device(&mut self, room_name: &str, unique_name: &str) -> HouseResult {
+        todo!()
+    }
+
+    pub fn remove_device(&mut self, room_name: &str, device_name: &str) -> Option<String> {
+        todo!()
+    }
+
+    pub fn get_device_list_in_room(&self, room_name: &str) -> Iter<String> {
+        todo!()
     }
 }
 
@@ -78,5 +90,35 @@ mod tests {
         assert!(room_list.next().map_or(false, |room| room.eq("R1")));
         assert!(room_list.next().map_or(false, |room| room.eq("R2")));
         assert!(room_list.next().is_none());
+    }
+
+    #[test]
+    fn test_device() {
+        let mut h = House::default();
+        assert!(h.add_room("R1").is_ok());
+        assert!(h.add_room("R2").is_ok());
+        assert!(h.add_device("R1", "Socket1").is_ok());
+        assert!(h.add_device("R1", "Socket2").is_ok());
+        assert!(h.add_device("R2", "Thermo").is_ok());
+        assert!(h.add_device("R2", "Socket1").is_ok());
+        let result = h.add_device("R1", "Socket2");
+        if let Err(e) = result {
+            if let HouseError::DeviceNameClash = e {
+                assert!(true);
+            } else {
+                panic!("Error should be DeviceClashError.");
+            }
+        } else {
+            panic!("There is the same device in the room but result is Ok.");
+        }
+        if let Err(e) = h.add_device("No such room", "Device") {
+            if let HouseError::NoRoomName = e {
+                assert!(true);
+            } else {
+                panic!("Error should be NoRoomName");
+            }
+        } else {
+            panic!("There is no room with this name.")
+        }
     }
 }
